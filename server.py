8f1a4fadf8f9e4213fc959a23637e09355f2369a
Jack Wang <jack.wangjiaqi.1996@gmail.com>
2018-06-05 18:42:34 -0400

    Basic sqlachemy connection setup.

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 '''Simple starter flask app'''
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import database
+
+database.import_restaurant()
 APP = Flask(__name__)
 
 
@@ -9,4 +13,21 @@ def hello_world():
     return 'Hello, World!'
 
 
-APP.run(host='0.0.0.0')
+@APP.route('/upload/', methods=['POST'])
+def photo_upload():
+    '''upload photo path'''
+    print(request.files)
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(filename)
+    return redirect(url_for('photo_upload', filename=filename))
+
+
+@APP.route('/new/restaurant/', methods=['POST'])
+def new_restaurant():
+    '''new restaurant scheme'''
+    print(request)
+    return 'Hello world'
+
+
+# APP.run(host='0.0.0.0')
