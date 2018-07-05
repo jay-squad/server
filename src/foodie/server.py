13889fa172bb7e2ec2cfe6f5b03c0654370095fe
Jack Wang <jack.wangjiaqi.1996@gmail.com>
2018-07-05 02:18:33 -0400

    restaurant search returns images for restaurant

--- src/foodie/server.py
+++ src/foodie/server.py
@@ -68,10 +68,17 @@ def get_restaurant_menu(restaurant_id):
 
 @APP.route('/search/restaurant/<query>', methods=['GET'])
 def search_restaurant(query):
-    return jsonify([
-        marshmallow_schema.RestaurantSchema().dump(restaurant).data
-        for restaurant in search.find_restaurant(query)
-    ])
+    return jsonify([{
+        "restaurant":
+        marshmallow_schema.RestaurantSchema().dump(restaurant).data,
+        "menu": [{
+            "item":
+            marshmallow_schema.MenuItemSchema().dump(item).data,
+            "image":
+            marshmallow_schema.ItemImageSchema().dump(image).data,
+        } for section in database.get_restaurant_menu_items(restaurant.id)
+                 for item, image in section[1]][:6]
+    } for restaurant in search.find_restaurant(query)])
 
 
 @APP.route('/search/item/<query>', methods=['GET'])
